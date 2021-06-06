ad8438e0585708394ffe90d4efc2d59698d0de1c
sazanami16 <za_nami.za_nami16@icloud.com>
2021-06-06 22:46:04 +0900

    continueとbreak

--- main.php
+++ main.php
@@ -1,15 +1,17 @@
 <?php
 
-$hp = 100;
+for ($i = 1; $i <= 10; $i++) {
+  // if ($i === 4) {
+  //   continue; //continueで4をスキップする記述。ループを一回スキップする処理。
+  // }
 
-while ($hp > 0) {
-  echo " Your HP: $hp" . PHP_EOL;
-  $hp -= 15;
+  // if ($i % 3 === 0) { //3の倍数をスキップする記述。
+  //   continue; 
+  // }
+
+  if ($i === 4) { //4に達すると4以降の数字は表示されなくなる。
+    break; //breakでループを抜け出す処理を記述。
+  } 
+  echo $i . PHP_EOL;
 }
 
-do {
-  echo " Your HP: $hp" . PHP_EOL;
-  $hp -= 15;
-} while ($hp > 0); 
-
-  
