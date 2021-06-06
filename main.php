db804309a5e81e97d729e8e1cd92a5ba29e49348
sazanami16 <za_nami.za_nami16@icloud.com>
2021-06-06 23:33:47 +0900

    ?型名

--- main.php
+++ main.php
@@ -1,12 +1,11 @@
 <?php
 
-declare(strict_types=1); //強い型付けの定義。暗黙のデータ変換が行われない。
+declare(strict_types=1); 
 
-function showInfo(string $name, int $score): void //弱い型付けの定義。暗黙のデータ変換が行われる。返り値も変更できるが、返り値がない場合は、「void」を使用する。
+function getAward(?int $score): ?string //「?」をつけると「null」か「string」で型付けを判断する。引数にも設定できる。「null」かしか判断できない。
 {
-  echo $name . ': ' . $score . PHP_EOL; 
+  return $score >= 100 ? 'Gold Medal' : null; //条件演算子
 }
 
-showInfo('tanaka', 40); 
-showInfo('tanaka', 'tanabe'); //型が異なるので、エラーとなる。
-showInfo('tanaka', '40'); //弱い型付けの場合は、暗黙の型変換が行われる。
+echo getAward(150) . PHP_EOL;
+echo getAward(40) . PHP_EOL;
