ebeaea803e65f8c787f840678bb841ecc7672352
sazanami16 <za_nami.za_nami16@icloud.com>
2021-06-06 23:17:27 +0900

    関数内の条件演算子

--- main.php
+++ main.php
@@ -3,12 +3,16 @@
 
 function sum($a, $b, $c)
 {
-  return $a + $b + $c;
+  $total =  $a + $b + $c;
+
+  // if ($total < 0) {
+  //   return 0;
+  // } else {
+  //   return $total;
+  // }
+  return $total < 0 ?  0 : $total; //if文の書き換え
+  // return 「$total < 0 ?」 ←条件式 「0」←「true」の場合 : 「$total」 ←「false」の場合;
 }
 
-
-$sum = function ($a, $b, $c) { //関数自体に名前がないものを「無名関数」もしくは「クロージャー」と呼ぶ。
-  return $a + $b + $c;
-}; //関数を値として代入する場合は、「;」は必要。
- 
-echo $sum(100, 300 ,500) . PHP_EOL;
+echo sum(100, 300, 500) . PHP_EOL;
+echo sum(-1000, 300, 500) . PHP_EOL;
