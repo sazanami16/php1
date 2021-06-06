afd5467ba0c99fb5588ea45d9a5700fc5088e09d
sazanami16 <za_nami.za_nami16@icloud.com>
2021-06-06 22:49:13 +0900

    関数の定義

--- main.php
+++ main.php
@@ -1,17 +1,16 @@
 <?php
 
-for ($i = 1; $i <= 10; $i++) {
-  // if ($i === 4) {
-  //   continue; //continueで4をスキップする記述。ループを一回スキップする処理。
-  // }
-
-  // if ($i % 3 === 0) { //3の倍数をスキップする記述。
-  //   continue; 
-  // }
-
-  if ($i === 4) { //4に達すると4以降の数字は表示されなくなる。
-    break; //breakでループを抜け出す処理を記述。
-  } 
-  echo $i . PHP_EOL;
+function showAd() 
+{
+  echo '----------' . PHP_EOL;
+  echo '----AD----' . PHP_EOL;
+  echo '----------' . PHP_EOL;
 }
 
+showAd();
+echo 'Tom is great!' . PHP_EOL;
+echo 'Bob is great!' . PHP_EOL;
+showAd();
+echo 'Steve is great!' . PHP_EOL;
+echo 'Bob is great!' . PHP_EOL;
+showAd();
