ea76df38fa93b67bac0b2b93e3eb1ff4c73f30a5
sazanami16 <za_nami.za_nami16@icloud.com>
2021-06-06 21:00:41 +0900

    ""内の記述方法

--- main.php
+++ main.php
@@ -3,4 +3,7 @@ $name = 'tanaka';
 $world = 'World';
 
 echo 'Hello ' . $name . PHP_EOL;
-echo 'Hello ' . $world . PHP_EOL;
+echo "IT's \"Hello\" .\t Hello $world" . PHP_EOL; 
+// 「\"\"」で""内でダブルクオテーションwp扱える。
+// tabを設定したい場合は、「\t」を使う。
+
