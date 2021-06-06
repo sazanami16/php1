742785226f02ef02963611b30de6b5c5a7a4c083
sazanami16 <za_nami.za_nami16@icloud.com>
2021-06-06 22:40:25 +0900

    while文

--- main.php
+++ main.php
@@ -1,6 +1,15 @@
 <?php
 
-for ($i = 1; $i <= 5; $i++) {
-  // echo 'Hello' . PHP_EOL;
-  echo "$i - Hello" . PHP_EOL;
+$hp = 100;
+
+while ($hp > 0) {
+  echo " Your HP: $hp" . PHP_EOL;
+  $hp -= 15;
 }
+
+do {
+  echo " Your HP: $hp" . PHP_EOL;
+  $hp -= 15;
+} while ($hp > 0); 
+
+  
