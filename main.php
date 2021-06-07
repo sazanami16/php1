8172accc5527015b60483e72e623df8cd7ddf680
sazanami16 <za_nami.za_nami16@icloud.com>
2021-06-07 12:26:57 +0900

    配列の要素を削除、追加。

--- main.php
+++ main.php
@@ -1,8 +1,8 @@
 <?php
 
-$scores = [30, 40, 50, 60, 70];
-$partial = array_slice($scores, 2); //scores[2]から末尾まで切り出す。末尾まで指定する場合は、桁数を省略できる。
-$partial = array_slice($scores, -2); //末尾から2つ切り出す。
+$scores = [30, 40, 50, 60, 70, 80];
+array_splice($scores, 2, 3);             //array_splice(配列, 位置, 個数)。配列の要素を削除できる。
+array_splice($scores, 2, 3, 100);        //array_splice(配列, 位置, 個数, 要素)。配列の要素を削除して、新しい要素を追加できる。
+array_splice($scores, 2, 0, [100, 110]); //array_splice(配列, 位置, 個数, [要素, 要素])。要素を複数追加する場合は、[]をつけて指定する必要がある。
 
 print_r($scores);
-print_r($partial);
