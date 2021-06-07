c75320581fb6d00bd22237c426e66b30ab4a3ca2
sazanami16 <za_nami.za_nami16@icloud.com>
2021-06-07 13:33:23 +0900

    array_map()の処理

--- main.php
+++ main.php
@@ -1,20 +1,12 @@
 <?php
 
-$a = [3, 4, 8, 9];
-$b = [4, 8, 12, 14];
+$prices = [100, 200, 300];
 
-$merged = array_merge($a, $b); //各配列を結合させる処理。
-// $merged = [...$a, ...$b];
-print_r($merged);
+$newPrices = array_map(
+  function ($n) { return $n * 1.1; }, 
+  // fn($n) => $n * 1.1, //returnで値を返すのみの処理の場合、アロー関数を使用できる。
+  $prices //第2引数
+);
 
-$uniques = array_unique($merged); //各配列の一意の値を出力させる処理。
-print_r($uniques);
+print_r($newPrices);
 
-$diff1 = array_diff($a, $b); //左記の場合、$aの配列と$bの配列の値を比較し、$aにしかない値を出力。
-print_r($diff1);
-
-$diff2 = array_diff($b, $a); //左記の場合、$bの配列と$aの配列の値を比較し、$bにしかない値を出力。
-print_r($diff2);
-
-$common = array_intersect($a, $b); //左記の場合、$aと$bの配列の値を比較し、共通する値を出力。
-print_r($common);
