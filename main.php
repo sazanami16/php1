5e197960371b28b91e5b237e1223846f84bfdc80
sazanami16 <za_nami.za_nami16@icloud.com>
2021-06-07 13:48:10 +0900

    キー、バリューの検索方法

--- main.php
+++ main.php
@@ -1,20 +1,24 @@
 <?php
 
-$numbers = range(1, 10);
+$scores = [
+  'taguchi' => 80,
+  'hayashi' => 70,
+  'kikuchi' => 60,
+];
 
-$eventNumbers = array_filter(
-  $numbers,
-  function ($n) {
-    // if ($n % 2 === 0) {
-    //   return true;
-    // } else {
-    //   return false;
-    // }
-    return $n % 2 === 0;
-  } 
-  // fn($n) => $n % 2 === 0
-);
+$keys = array_keys($scores); //キーのみ取り出す処理。
+print_r($keys);
 
-print_r($eventNumbers);
+$values = array_values($scores); //バリューのみ取り出す処理。
+print_r($values);
+
+if (array_key_exists('taguchi', $scores) === true) { //配列内に該当のキーが存在するか調べる処理。
+  echo 'taguchi is here!' . PHP_EOL;
+}
+
+if (in_array(80, $scores) === true) { //配列内に該当の値が存在するか調べる処理。
+  echo '80 is here!' . PHP_EOL;
+}
 
 
+echo array_search(70, $scores) . PHP_EOL; //値を検索して対応するキーを調べる処理。
