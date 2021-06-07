21cbab5053fcd6b4e756dcd540cdd58b5368b826
sazanami16 <za_nami.za_nami16@icloud.com>
2021-06-07 11:49:40 +0900

    substr(), substr_replace()

--- main.php
+++ main.php
@@ -1,9 +1,19 @@
 <?php
 
-$input = ' dot_taguchi ';
-$input = trim($input);
+// substr(文字列, 位置, 桁数)：文字を切り出しを指定できる処理。
+// substr_replace(文字列, 置換文字列, 位置, 桁数)：切り出した文字を置換できる処理。
 
-echo strlen($input) . PHP_EOL;
-echo strpos($input, '_'). PHP_EOL;
-$input = str_replace('_', '-', $input);
-echo $input . PHP_EOL;
+
+$input = '20200320Item-A  1200';
+$input = substr_replace($input, 'Item-B  ', 8, 8);
+
+$date = substr($input, 0, 8);
+$product = substr($input, 8, 8);
+
+// $amount = substr($input,16 ,4); //最後の桁数まで切り出す場合は、桁数を省略できる。
+$amount = substr($input,16);
+
+echo $date . PHP_EOL;
+echo $product . PHP_EOL;
+// echo $amount . PHP_EOL;
+echo number_format($amount) . PHP_EOL; //number_format()で桁にカンマをつける。
