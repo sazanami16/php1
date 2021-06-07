1ecf8b89420d0ca5ce9d1abb83824e654fc83824
sazanami16 <za_nami.za_nami16@icloud.com>
2021-06-07 12:08:38 +0900

    数学系の処理

--- main.php
+++ main.php
@@ -1,8 +1,15 @@
 <?php
 
-$d = [2020, 11, 15];
-echo "$d[0]-$d[1]-$d[2]" . PHP_EOL;
-echo implode('-', $d) . PHP_EOL; //配列の要素を文字列にする。
+$n = 5.6283;
 
-$t = '17:32:45';
-print_r(explode(':', $t)); //文字列を配列にする。
+echo ceil($n) . PHP_EOL; //切り上げ
+echo floor($n) . PHP_EOL; //切り捨て
+echo round($n) . PHP_EOL; //整数で四捨五入
+echo round($n, 2) . PHP_EOL; //小数点第2位以下で四捨五入
+
+echo mt_rand(1, 6) .PHP_EOL; //()内で指定された数値の範囲内でランダムで数値が出力される。
+echo max(1, 4, 6) .PHP_EOL; //()内の最大値が出力。
+echo min(1, 4, 6) .PHP_EOL; //()内の最小値が出力。
+
+echo M_PI . PHP_EOL; //円周率
+echo M_SQRT2 .PHP_EOL; //2の平方根
