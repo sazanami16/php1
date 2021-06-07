1d0f5b155c4a2fb97c82129b917b901de8743707
sazanami16 <za_nami.za_nami16@icloud.com>
2021-06-07 13:38:28 +0900

    array_filter

--- main.php
+++ main.php
@@ -1,12 +1,20 @@
 <?php
 
-$prices = [100, 200, 300];
+$numbers = range(1, 10);
 
-$newPrices = array_map(
-  function ($n) { return $n * 1.1; }, 
-  // fn($n) => $n * 1.1, //returnで値を返すのみの処理の場合、アロー関数を使用できる。
-  $prices //第2引数
+$eventNumbers = array_filter(
+  $numbers,
+  function ($n) {
+    // if ($n % 2 === 0) {
+    //   return true;
+    // } else {
+    //   return false;
+    // }
+    return $n % 2 === 0;
+  } 
+  // fn($n) => $n % 2 === 0
 );
 
-print_r($newPrices);
+print_r($eventNumbers);
+
 
