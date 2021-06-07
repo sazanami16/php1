7a2dbb03f9806fd33ceebe907c9034a5b0c879f5
sazanami16 <za_nami.za_nami16@icloud.com>
2021-06-07 12:19:50 +0900

    array_slice()

--- main.php
+++ main.php
@@ -1,12 +1,8 @@
 <?php
 
-$scores = [30, 40, 50];
-array_unshift($scores, 10, 20); //先頭に要素を追加。
-array_push($scores, 60, 70); //最後尾に要素を追加。
-$scores[] = 80;
-
-array_shift($scores); //要素の先頭を削除。
-array_pop($scores); //要素の最後尾を削除。
+$scores = [30, 40, 50, 60, 70];
+$partial = array_slice($scores, 2); //scores[2]から末尾まで切り出す。末尾まで指定する場合は、桁数を省略できる。
+$partial = array_slice($scores, -2); //末尾から2つ切り出す。
 
 print_r($scores);
-
+print_r($partial);
