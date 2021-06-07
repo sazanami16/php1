53d5572530148d52b7fbd540ef9b4811296d1719
sazanami16 <za_nami.za_nami16@icloud.com>
2021-06-07 12:13:51 +0900

    配列の要素の操作

--- main.php
+++ main.php
@@ -1,15 +1,12 @@
 <?php
 
-$n = 5.6283;
+$scores = [30, 40, 50];
+array_unshift($scores, 10, 20); //先頭に要素を追加。
+array_push($scores, 60, 70); //最後尾に要素を追加。
+$scores[] = 80;
 
-echo ceil($n) . PHP_EOL; //切り上げ
-echo floor($n) . PHP_EOL; //切り捨て
-echo round($n) . PHP_EOL; //整数で四捨五入
-echo round($n, 2) . PHP_EOL; //小数点第2位以下で四捨五入
+array_shift($scores); //要素の先頭を削除。
+array_pop($scores); //要素の最後尾を削除。
 
-echo mt_rand(1, 6) .PHP_EOL; //()内で指定された数値の範囲内でランダムで数値が出力される。
-echo max(1, 4, 6) .PHP_EOL; //()内の最大値が出力。
-echo min(1, 4, 6) .PHP_EOL; //()内の最小値が出力。
+print_r($scores);
 
-echo M_PI . PHP_EOL; //円周率
-echo M_SQRT2 .PHP_EOL; //2の平方根
