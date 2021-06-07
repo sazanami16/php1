5a86b86ca35641f5a15c8d63bcdc5420febe67f2
sazanami16 <za_nami.za_nami16@icloud.com>
2021-06-07 11:36:58 +0900

    文字変換

--- main.php
+++ main.php
@@ -1,18 +1,9 @@
 <?php
 
-function getStats(...$numbers) //$numbers = [1, 3, 5]と考えられる。
-{
-  $total = 0;
-  foreach ($numbers as $number) {
-    $total += $number;
-  }
-  return [$total, $total / count($numbers)]; //返り値を配列にして戻すことができる。[$total //(← $sum), $total / count($numbers) //(← $average)]
-}
+$input = ' dot_taguchi ';
+$input = trim($input);
 
-// print_r(getStats(1, 3, 5));
-
-list($sum, $average) = getStats(1, 3, 5); //「list」によって、分割代入ができるので、()内の要素をそれぞれの変数に戻すことができる。
-[$sum, $average] = getStats(1, 3, 5); //上記の「list」と同じ処理を行う。
-
-echo $sum . PHP_EOL;
-echo $average . PHP_EOL;
+echo strlen($input) . PHP_EOL;
+echo strpos($input, '_'). PHP_EOL;
+$input = str_replace('_', '-', $input);
+echo $input . PHP_EOL;
