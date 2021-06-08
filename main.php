9cb4157292e038bc0f93d7fe60d29d93931e786c
sazanami16 <za_nami.za_nami16@icloud.com>
2021-06-08 13:49:58 +0900

    抽象クラスと型の継承

--- main.php
+++ main.php
@@ -1,48 +1,69 @@
 <?php
 
-class Post //継承元のクラス（親クラス）
+abstract class BasePost //抽象クラス
 {
-  protected $text; //「protected」によって継承したクラス内でも使用できる。クラス外では使用不可。
+  abstract public function show(); //抽象メソッド
 
+  protected $text;
+  
   public function __construct($text)
   {
     $this->text = $text;
   }
+}
 
-  final public function show() //「final」をつけてオーバーライドを防ぐ。
+class Post extends BasePost
+{
+
+  public function show()
   {
     printf('%s' . PHP_EOL, $this->text);
   }
 }
 
-class SponsoredPost extends Post //継承先のクラス(子クラス)
+class SponsoredPost extends BasePost
 {
   private $sponsor;
-
+  
   public function __construct($text, $sponsor)
   {
     parent::__construct($text);
     $this->sponsor = $sponsor;
   }
-
-  public function showSponsor()
-  {
-    printf('%s' . PHP_EOL, $this->sponsor);
-  }
-
-  //メソッドのオーバーライド
+  
   public function show()
   {
     printf('%s by %s' . PHP_EOL, $this->text, $this->sponsor);
   }
 }
 
+class PremiumPost extends BasePost
+{
+  private $price;
+  
+  public function __construct($text, $price)
+  {
+    parent::__construct($text);
+    $this->price = $price;
+  }
+  
+  public function show()
+  {
+    printf('%s [%d JPY]' . PHP_EOL, $this->text, $this->price);
+  }
+}
+
 $posts = [];
 $posts[0] = new Post('hello');
 $posts[1] = new Post('hello again');
-$posts[2] = new SponsoredPost('hello world', 'tanabe');
+$posts[2] = new SponsoredPost('hello hello', 'tanaka');
+$posts[3] = new PremiumPost('hello world', 300);
+function processPost(BasePost $post)
+{
+  $post->show();
+}
 
-$posts[0]->show();
-$posts[1]->show();
-$posts[2]->show();
-$posts[2]->showSponsor();
+foreach ($posts as $post) {
+  processPost($post);
+}
+
