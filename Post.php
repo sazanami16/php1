6ad394b5dd058d16d2c572f710bd444560e1796c
sazanami16 <za_nami.za_nami16@icloud.com>
2021-06-08 14:25:17 +0900

    namespace

---
+++ Post.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace zazaza\MyPHPApp;
+
+class Post
+{
+  private $text;
+
+  function __construct($text)
+  {
+    $this->text = $text;
+  }
+
+  function show()
+  {
+    printf('%s' . PHP_EOL, $this->text);
+  }
+}
--- main.php
+++ main.php
@@ -1,101 +1,19 @@
 <?php
 
-trait LikeTrait
-{
-  private $likes = 0;
+use zazaza\MyPHPApp as zazaza;
 
-  public function like() 
-  {
-    $this->likes++;
-  }
-}
+require('Post.php'); //処理が止まる
+// include('post.php'); //処理が止まらない
 
+// spl_autoload_register(function ($class) { //読み込むファイルが多くなってきたときに自動的に複数のファイルお読み込ませる仕組み。
+//   require($class . '.php');
+// });
 
-interface LikeInterface  //実装を含めてはいけない
-{
-  public function like();
-}
+class Post {}
 
-abstract class BasePost //抽象クラス
-{
-  abstract public function show(); //抽象メソッド
-
-  protected $text;
-  
-  public function __construct($text)
-  {
-    $this->text = $text;
-  }
-}
-
-
-class Post extends BasePost implements LikeInterface
-{
-  use LikeTrait;
-
-  public function show()
-  {
-    printf('%s (%d)' . PHP_EOL, $this->text, $this->likes);
-  }
-}
-
-
-class SponsoredPost extends BasePost
-{
-  private $sponsor;
-  
-  public function __construct($text, $sponsor)
-  {
-    parent::__construct($text);
-    $this->sponsor = $sponsor;
-  }
-  
-  public function show()
-  {
-    printf('%s by %s' . PHP_EOL, $this->text, $this->sponsor);
-  }
-}
-
-
-class PremiumPost extends BasePost implements LikeInterface
-{
-  private $price;
-
-  use LikeTrait;
-  
-  public function __construct($text, $price)
-  {
-    parent::__construct($text);
-    $this->price = $price;
-  }
-  
-  public function show()
-  {
-    printf('%s (%d) [%d JPY]' . PHP_EOL, $this->text, $this->likes, $this->price);
-  }
-}
-
-
-$posts = [];
-$posts[0] = new Post('hello');
-$posts[1] = new Post('hello again');
-$posts[2] = new SponsoredPost('hello hello', 'tanaka');
-$posts[3] = new PremiumPost('hello world', 300);
-
-function processLikeable(LikeInterface $likeable)
-{
-  $likeable->like();
-}
-
-processLikeable($posts[0]);
-processLikeable($posts[3]);
-
-function processPost(BasePost $post)
-{
-  $post->show();
-}
+$posts[0] = new zazaza\Post('hello');
+$posts[1] = new zazaza\Post('hello again');
 
 foreach ($posts as $post) {
-  processPost($post);
+  $post->show();
 }
-
